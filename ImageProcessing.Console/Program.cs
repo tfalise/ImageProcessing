bec90579fe0c9776b9dc0b53d21d1bd38da9069c
Thibault Falise <thibault.falise@gmail.com>
2019-03-13 15:22:11 +0100

    Add console application to test transformation

---
+++ ImageProcessing.Console/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageProcessing.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var source = new MyImage(args[0]);
+            var transformation = new BlackAndWhiteTransformation();
+            var result = transformation.Process(source);
+            result.WriteToFile(args[1]);
+        }
+    }
+}
