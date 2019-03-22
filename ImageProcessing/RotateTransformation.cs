a69945768ab2f738c35da7ea0e27186f360c6cf4
Thibault Falise <thibault.falise@gmail.com>
2019-03-22 17:57:23 +0100

    Refactor rotation algorithm

--- ImageProcessing/RotateTransformation.cs
+++ ImageProcessing/RotateTransformation.cs
@@ -1,8 +1,19 @@
-﻿namespace ImageProcessing
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace ImageProcessing
 {
     public class RotateTransformation
     {
         private readonly ImageRotation _rotation;
+        private readonly Dictionary<ImageRotation, Func<int,int,MyImage,Pixel>> _rotationMappings = new Dictionary<ImageRotation, Func<int, int, MyImage, Pixel>>()
+        {
+            [ImageRotation.None] = (row, column, image) => image[row, column],
+            [ImageRotation.Clockwise90] = (row, column, image) => image[image.Height - 1 - column, row],
+            [ImageRotation.Clockwise180] = (row, column, image) => image[image.Height - 1 - row, image.Width - 1 - column],
+            [ImageRotation.Clockwise270] = (row, column, image) => image[column, image.Width - 1 - row],
+        };
 
         public RotateTransformation(ImageRotation rotation)
         {
@@ -13,39 +24,11 @@
         {
             var result = CreateRotatedCanvas(source);
 
-            if (_rotation == ImageRotation.Clockwise90)
+            for (var row = 0; row < result.Height; row++)
             {
-                for (var row = 0; row < result.Height; row++)
+                for (var column = 0; column < result.Width; column++)
                 {
-                    for (var column = 0; column < result.Width; column++)
-                    {
-                        var sourcePixel = source[source.Height - 1 - column, row];
-                        result[row, column] = sourcePixel;
-                    }
-                }
-            }
-
-            if (_rotation == ImageRotation.Clockwise180)
-            {
-                for (var row = 0; row < result.Height; row++)
-                {
-                    for (var column = 0; column < result.Width; column++)
-                    {
-                        var sourcePixel = source[source.Height - 1 - row, source.Width - 1 - column];
-                        result[row, column] = sourcePixel;
-                    }
-                }
-            }
-
-            if (_rotation == ImageRotation.Clockwise270)
-            {
-                for (var row = 0; row < result.Height; row++)
-                {
-                    for (var column = 0; column < result.Width; column++)
-                    {
-                        var sourcePixel = source[column, source.Width - 1 - row];
-                        result[row, column] = sourcePixel;
-                    }
+                    result[row, column] = _rotationMappings[_rotation](row, column, source);
                 }
             }
 
