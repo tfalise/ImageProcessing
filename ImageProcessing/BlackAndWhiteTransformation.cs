ccc44bd4365ee31e2c8bf2530b75d3b2ce9651e8
FALISE Thibault <thibault-externe.falise@edf.fr>
2019-03-26 18:06:53 +0100

    Refactor black & white transformation algorithms

--- ImageProcessing/BlackAndWhiteTransformation.cs
+++ ImageProcessing/BlackAndWhiteTransformation.cs
@@ -1,10 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ImageProcessing
 {
     public class BlackAndWhiteTransformation
     {
         private BlackAndWhiteAlgorithm _algorithm;
+
+        private static readonly Dictionary<BlackAndWhiteAlgorithm, Func<Pixel, int>> GrayLevels = new Dictionary<BlackAndWhiteAlgorithm, Func<Pixel, int>>() 
+        {
+            [BlackAndWhiteAlgorithm.Average] = (pixel) => (pixel.Red + pixel.Green + pixel.Blue) / 3,
+            [BlackAndWhiteAlgorithm.Lightness] = (pixel) => (Math.Max(Math.Max(pixel.Red, pixel.Blue), pixel.Green) + Math.Min(Math.Min(pixel.Red, pixel.Blue), pixel.Green)) / 2,
+            [BlackAndWhiteAlgorithm.Luminosity] = (pixel) => (int)(0.21 * pixel.Red + 0.07 * pixel.Blue + 0.72 * pixel.Green)
+        };
 
         public BlackAndWhiteTransformation() : this(BlackAndWhiteAlgorithm.Average)
         {
@@ -21,23 +29,8 @@ namespace ImageProcessing
             var result = new MyImage(source.Width, source.Height);
             for (var i = 0; i < result.Pixels.Length; i++)
             {
-                if(_algorithm == BlackAndWhiteAlgorithm.Average)
-                {
-                    var grayLevel = (source.Pixels[i].Red + source.Pixels[i].Blue + source.Pixels[i].Green) / 3;
-                    result.Pixels[i] = new Pixel(grayLevel, grayLevel, grayLevel);
-                }
-                else if(_algorithm == BlackAndWhiteAlgorithm.Lightness)
-                {
-                    var maxRGB = Math.Max(Math.Max(source.Pixels[i].Red, source.Pixels[i].Blue), source.Pixels[i].Green);
-                    var minRGB = Math.Min(Math.Min(source.Pixels[i].Red, source.Pixels[i].Blue), source.Pixels[i].Green);
-                    var grayLevel = (maxRGB + minRGB) / 2;
-                    result.Pixels[i] = new Pixel(grayLevel, grayLevel, grayLevel);
-                }
-                else 
-                {
-                    var grayLevel = (int)(0.21 * source.Pixels[i].Red + 0.07 * source.Pixels[i].Blue + 0.72 * source.Pixels[i].Green);
-                    result.Pixels[i] = new Pixel(grayLevel, grayLevel, grayLevel);
-                }
+                var grayLevel = GrayLevels[_algorithm](source.Pixels[i]);
+                result.Pixels[i] = new Pixel(grayLevel, grayLevel, grayLevel);
             }
             return result;
         }
